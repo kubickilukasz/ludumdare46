0b939b01dc8b5a64e11d527ace1a33100bc18f30
Kubeec <kubicki.lukasz98@gmail.com>
2020-04-18 08:46:01 +0200

    first movement

---
+++ ludumdare46/Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 500f;
+
+    [SerializeField]
+    float smoothTime = 0.7f;
+
+    Vector3 direction = Vector3.zero;
+
+    Vector3 vDirection;
+    Vector3 currentDirection;
+
+      public Vector3 Direction
+    {
+        get
+        {
+            return currentDirection;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        direction += new Vector3(-Input.GetAxis("Mouse Y") , Input.GetAxis("Mouse X") , 0 ) *speed * Time.deltaTime;
+
+        direction.x = Mathf.Clamp(direction.x, -80, 70);
+
+        currentDirection = Vector3.SmoothDamp(currentDirection , direction , ref vDirection ,smoothTime);
+
+        transform.localRotation = Quaternion.Euler(new Vector3(currentDirection.x, 0, 0));
+
+        
+    }
+}
---
+++ ludumdare46/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+   [SerializeField]
+    CameraMovement myCamera;
+    
+    [SerializeField]
+    float speed = 5f;
+
+    [SerializeField]
+    float smooth = 0.1f;
+
+    [SerializeField]
+    float gravity = -9f;
+
+     [SerializeField]
+    float jump = 5f;
+
+
+    CharacterController characterController;
+
+    Vector3 currentDirection;
+    Vector3 vDirection;
+
+    float _yVelocity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentDirection.y = 0;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0 , myCamera.Direction.y , 0));
+        
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal") , 0 , Input.GetAxisRaw("Vertical"));
+
+        direction = myCamera.transform.rotation * direction;
+
+        direction = direction.normalized;
+
+        direction *= speed * Time.deltaTime;
+
+        currentDirection = Vector3.SmoothDamp(currentDirection , direction , ref vDirection , smooth);
+
+        if(characterController.isGrounded){
+
+            if(Input.GetButtonDown("Jump")){
+                _yVelocity = Mathf.Sqrt(jump * -2.0f * gravity) * Time.deltaTime;
+            }else{
+                _yVelocity = -0.01f;
+            }
+
+        }else{
+            _yVelocity += gravity * Time.deltaTime * Time.deltaTime;
+        }
+
+        currentDirection.y = _yVelocity;
+
+        characterController.Move(currentDirection);
+
+
+    }
+}
